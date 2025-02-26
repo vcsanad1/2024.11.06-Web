e26816f04cbab89130566b5c79ea2ada38a56dbb
veres.csanad <veres.csanad@ckik.hu>
2025-02-26 13:19:50 +0100

    haha

--- Fuggvenyek/index.js
+++ Fuggvenyek/index.js
@@ -5,12 +5,52 @@ function ertekekMegadasa() {
     console.log(`${a} ${b}`)
 }
 
-function random() {
-    return Math.floor(Math.random()*(b-a+1)+a);
+
+function tombgeneralas(n){
+let t=[];
+for (let i = 0; i < n; i++) {
+    t.push(Math.floor(Math.random()*10));
 }
+return t;
+}
+function Tombatlag(tombb) {
+    let t=[];
+for (let i = 0; i < 13; i++) {
+    t.push(Math.floor(Math.random()*10));
+    let atlag=i%t;
+    tombb=atlag;
+}
+return t;
+}
+function VanEnulla(tombb) {
+    let vanenulla=false;
+        if (tombb[i]===0) {
+            vanenulla=true;
+        }
+    }
+
 
 function main() {
     ertekekMegadasa();
-    random();
+    let randomm=Math.random(10);
+    console.log(randomm)
+
+    let tombb=tombgeneralas(13);
+    console.log(tombb);
+
+    VanEnulla(tombb)
+    console.log(VanEnulla(tombb,0))
+    console.log(Tombatlag(tombb));
+    
+
 }
 main();
+
+
+
+
+
+
+
+
+
