33ec148338d20f3a79e698d75d90fb7630e29afd
veres.csanad <veres.csanad@ckik.hu>
2025-03-31 08:45:23 +0200

    waeawe

--- Objektumok/script2.js
+++ Objektumok/script2.js
@@ -1,5 +1,68 @@
 // console.log(adatok)
-function main() {
-    
+function SzovegfelDolgoz(adatok) {
+    const tomb=szovegDarabol(adatok,";");
+    console.table(tomb);
+    let adat=[];
+    for (let i = 0; i < tomb.length; i++) {
+        const st=szovegDarabol(tomb[i]," ")
+        adat.push({
+            korzet:parseInt(st[0]),
+            szavazatok:st[1],
+            veznev:st[2],
+            kernev:st[3],
+            part:st[4]
+        })
+    }
+    return adat;
 }
-main();
+function szovegDarabol(adatok,elvalaszt) {
+    let darabok=[];
+    let text="";
+    for (let i = 0; i < adatok.length; i++) {
+        if (adatok[i]!=elvalaszt) {
+            text+=adatok[i];
+        }
+        else{
+            darabok.push(text);
+            text="";
+        }
+    }
+    darabok.push(text);
+    return darabok;
+}
+
+function MEgszamol(darabol) {
+    let db=0;
+    for (let i = 0; i < darabol.length; i++) {
+            db++;
+    }
+    return db;
+}
+
+function Szavazat(darabol,vezeteknev,kereszt) {
+    let table=[];
+    let i=0;
+    while (i<darabol.length&&!(darabol[i].veznev===vezeteknev&&darabol[i].kernev===kereszt)) {
+    i++;
+    }
+    if (i>=darabol.length) {
+        return -1;
+    }
+    else{
+        return [darabol[i].veznev,darabol[i].kernev]
+    }
+}
+
+function main(adatok) {
+    const darabol=SzovegfelDolgoz(adatok);
+    console.table(darabol);
+
+    const xd=MEgszamol(darabol);
+    console.log(`A helyi választason ${xd}-ember indult.`);
+    
+    const vezeteknev="Simon";
+    const kereszt="Simon";
+    const vezkereszt=Szavazat(darabol,vezeteknev,kereszt);
+    console.log(vezkereszt);
+}
+main(adatok);
---
+++ rejtveny/beolv.js
@@ -0,0 +1,24 @@
+function Szoveg(szoveg) {
+    const sorok=szoveg.split(';')
+    console.log(sorok);
+    const n=parseInt(sorok[0]);
+    let adatok=[];
+    for (let i = 1; i < n.length; i+=11) {
+            const adat={
+                nev:sorok[i],
+                matrix: matrixKialakitas()
+            }
+
+
+        adatok.push(adat)
+    }
+}
+
+
+
+
+
+function main() {
+    const beolv=Szoveg(szoveg);
+}
+main();
